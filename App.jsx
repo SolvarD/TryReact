4be745e0080a700b36240d9fdd75bddfc2ff27a7
SolvarD <solvar@msn.com>
2017-11-06 22:51:44 +0100

    first

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+
+        var g = 1;
+
+      return (
+         <div>
+            Hello World!!!
+
+          { g }
+         </div>
+      );
+   }
+}
+
+export default App;
